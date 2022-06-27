72f3473ed1d2e443449354e19178fcde096955b7
Intel <Intel@DESKTOP-SLIGSU6>
2022-06-27 12:36:40 +0530

    Variables &Datatypes

---
+++ Java introduction code/src/Intro/Hello1.java
@@ -0,0 +1,19 @@
+package Intro;
+
+public class Hello1 {
+
+	public static void main(String[] args) 
+	{
+		int max = 2147483647;
+		int min = 2147483646;
+		short shortMax = 32767;
+		short shortMin = -32768;
+		long longMax = 922337203684775807L;
+		long longMin = -9223372036854775808L;
+		byte byteMax = 127;
+		byte byteMin = -128;
+		
+
+	}
+
+}
---
+++ Java introduction code/src/Intro/Hello4.java
@@ -0,0 +1,17 @@
+package Intro;
+
+public class Hello4
+{
+
+	public static void main(String[] args) 
+	{
+		String var = new String("Hello World");
+		System.out.println(var);
+	}
+	
+}
+
+
+	
+
+
